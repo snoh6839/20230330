7885da33589c4381daf42637c5a19382b5733c8c
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-30 12:09:14 +0900

    구현끝

--- sub_string.php
+++ sub_string.php
@@ -46,6 +46,24 @@ print_r($helloArr);
 //for랑 if로 돌려서  $stringArr[$index] == $replacement1 면 $replacement2로 바뀌게 해서 인덱스를 몰라도
 // 바꾸고 싶은 문자열을 내가 지정한 문자열로 바꾸는 함수 replaceAtString
 
+echo "\n--------------------\n";
+
+function replaceAtString($string, $search, $replacement1, $replacement2)
+{
+    $stringArr = explode($search, $string);
+    for($i = 0; $i < count($stringArr); $i++) {
+        if($stringArr[$i] == $replacement1) {
+            $stringArr[$i] = $replacement2;
+        }
+    }
+    return implode($search, $stringArr);
+}
+
+$happyArr = "I am always Happy.";
+$helloArr = replaceAtString($happyArr, " ", "Happy.", "Hello.");
+print_r($helloArr);
+
+echo "\n--------------------\n";
 
 
 //echo "\n--------------------\n";
@@ -53,6 +71,29 @@ print_r($helloArr);
 //$happexpl = explode("$replacement1", $str_all);
 //$happimpl = implode("$replacement2", $happexpl);
 //의 원리를 써서 같은 기능으로 구현
+
+echo "\n--------------------\n";
+
+function replaceAtString($string, $search, $replacement1, $replacement2)
+{
+    $stringArr = explode($search, $string);
+    $tempArr = array();
+    foreach($stringArr as $element){
+        if($element == $replacement1){
+            array_push($tempArr, $replacement2);
+        } else {
+            array_push($tempArr, $element);
+        }
+    }
+    return implode($search, $tempArr);
+}
+
+$happyArr = "I am always Happy.";
+$helloArr = replaceAtString($happyArr, " ", "Happy.", "Hello.");
+print_r($helloArr);
+
+echo "\n--------------------\n";
+
 
 
 
